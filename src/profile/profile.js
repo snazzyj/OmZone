f881431bb78b6ad75d088f4fbe49c7a37f970c91
AJ Olson <silentx.alex@gmail.com>
2020-03-17 12:59:20 -0400

    updated profile section to include more stats

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import BarGraph from '../bargraph/bargraph';
 import MeditationContext from '../meditationcontext';
 import './profile.css'
@@ -7,16 +7,16 @@ class Profile extends Component {
 
     static contextType = MeditationContext;
 
-    countTotalMinutes = (user) => {
+    count7DayTotalMinutes = (user) => {
 
-        if(user.medData.length !== 0) {
+        if (user.medData.length !== 0) {
             let total = user.medData
-            .map((element) => {
-                return element.minutes
-            })
-            .reduce((prev, curr) => {
-                return prev+curr
-            })
+                .map((element) => {
+                    return element.Minutes
+                })
+                .reduce((prev, curr) => {
+                    return prev + curr
+                })
             return total
         } else {
             return 0;
@@ -24,38 +24,62 @@ class Profile extends Component {
     }
 
     getAverage = (user) => {
-        if(user.medData.length !== 0 ) {
+        if (user.medData.length !== 0) {
             let average = user.medData
-            .map((element) => {
-                return element.minutes
-            })
-            .reduce((prev, curr) => {
-                return prev+curr
-            }) / user.medData.length;
-    
+                .map((element) => {
+                    return element.Minutes
+                })
+                .reduce((prev, curr) => {
+                    return prev + curr
+                }) / user.medData.length;
+
             return Math.round(average);
         } else {
             return 0;
         }
     }
 
-    render() {
-        const {user} = this.context;
+    convertMinutes = (user) => {
+        const { totalTime } = user;
+        let days = Math.floor(totalTime / 1440);
+        let hours = Math.floor((totalTime % 1440) / 60);
+        let mins = totalTime % 60;
 
-        return(
+        let data = {
+            Days: days,
+            Hours: hours,
+            Minutes: mins
+        }
+        return data
+    }
+
+    render() {
+        const { user } = this.context;
+        const time = this.convertMinutes(user);
+
+        return (
             <section className="profileSection">
                 <div>
-                <BarGraph />
+                    <BarGraph />
                 </div>
-                <div>
-                    <h3>Last 7 Meditations</h3>
-                    <p>
-                        Total time spent Meditating: {this.countTotalMinutes(user)} minutes
+                <section>
+                    <h1>Stats</h1>
+                    <div>
+                        <h3>Lifetime</h3>
+                        <p>Days: {time.Days}</p>
+                        <p>Hours: {time.Hours}</p>
+                        <p>Minutes: {time.Minutes}</p>
+                    </div>
+                    <div>
+                        <h3>Last 7 Meditations</h3>
+                        <p>
+                            Total time spent Meditating: {this.count7DayTotalMinutes(user)} minutes
                     </p>
-                    <p>
-                        Average time spent Meditating: {this.getAverage(user)} minutes
+                        <p>
+                            Average time spent Meditating: {this.getAverage(user)} minutes
                     </p>
-                </div>
+                    </div>
+                </section>
             </section>
         )
     }
