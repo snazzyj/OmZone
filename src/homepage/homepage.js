dac406813207df9c2c855115f9faee819c88ff74
AJ Olson <silentx.alex@gmail.com>
2020-04-08 14:21:02 -0400

    added styling and title

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faOm } from '@fortawesome/free-solid-svg-icons'
 import Login from '../login/login';
 import Signup from '../signup/signup';
 import Timer from '../timer/timer';
-import DemoLogin from '../demo/demoLogin';
+import Demo from '../demo/demo';
 import MeditationContext from '../meditationcontext';
+import './homepage.css'
+
 
 class Homepage extends Component {
 
@@ -13,19 +18,42 @@ class Homepage extends Component {
         const { isLoggedIn } = this.context.user
 
         return (
+            <Fade top cascade duration={1700}>
             <div>
                 {isLoggedIn
-                    ? <Timer />
+                    ? <div>
+                        <Timer />
+                    </div>
                     : <div>
-                        <Login />
-                        <Signup />
-                        <DemoLogin />
+                        <div className="navbar">
+                            <nav>
+                                <Login />
+                            </nav>
+                        </div>
+                        <section className="container">
+                            <div className="title">
+                                <h1>Om Zone</h1>
+                                <p>"Sleep is the best meditation"</p>
+                                <p>-Dalai Lama</p>
+                                <FontAwesomeIcon icon={faOm} size="4x" />
+                            </div>
+
+                            <div className="signup">
+                                <Signup />
+                            </div>
+                        </section>
+                        <div>
+                            <Demo />
+                        </div>
                     </div>
                 }
             </div>
+        </Fade>
         )
     }
 
 }
+
+
 
 export default Homepage;
