f5c03ba3b71c2acb3e83724900b2b7758792b69f
AJ Olson <silentx.alex@gmail.com>
2020-04-15 23:19:20 -0400

    added app description

--- src/demo/demoInfo.js
+++ src/demo/demoInfo.js
@@ -5,10 +5,16 @@ class DemoInfo extends Component {
     render() {
         return (
             <Fragment>
+                <div className="description">
+                    <p>
+                    Getting into meditation has never been easier. Track your progress, 
+                    select a sound cue for the start and end of your time 
+                    and even select a background which features an audio track      
+                    </p>
+                </div>
                 <Popup trigger={<button>Getting Started</button>} position="top center" closeOnDocumentClick>
                         <ol>
                             <li>Sign up for an account or Login in to continue</li>
-                            <li>Input the time you want to meditate for in minutes</li>
                             <li>Input the time you want to meditate for in minutes</li>
                             <li>Select a sound cue for when the timer starts and stops</li>
                             <li>By selecting a background, a song will be played</li>
